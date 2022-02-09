5daa1ce5d974cd3fc67e0cab42c53fc43c87f915
John Xanthopoulos <johxan@Johns-MBP.local>
2022-02-09 09:23:32 -0500

    changed project images

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ const IndexPage = () => {
         <div className="text-center hero-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">John Xanthopoulos</h1>
-            <p className="mb-5">IT Executive by day ...</p>
-            <p className="mb-5">... Developer on the weekend</p>
+            <p className="mb-5 text-2xl">IT Executive by day ...</p>
+            <p className="mb-5 text-2xl">... Developer on the weekend</p>
           </div>
         </div>
       </div>
